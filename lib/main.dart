46b5cb50de10e62b8fc204e85353dce8699aeac1
tianlin <li.tian.mc@qq.com>
2022-03-03 10:08:39 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tl_flutter_common/start/main.dart';
+
+import 'package:flutter_biz_main/main.dart';
+import 'package:flutter_biz_setting/main.dart';
+
+final Map<String, WidgetBuilder> visitRoutes = {
+  ...mainRoutes,
+};
+
+final Map<String, WidgetBuilder> authRoutes = {
+  ...settingRoutes,
+  ...commonRoutes
+};
+
+void main() async {
+  // read app setting data
+  runApp(MyApp(visitRoutes, authRoutes, const {
+    'theme': 0,
+    'language': 'en-US'
+  }));
+}
+
